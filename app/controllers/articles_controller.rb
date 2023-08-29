c6368209534dc002da4b36e4a712334c6635e0e4
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2023-08-28 22:30:29 -0700

    added comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,41 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title:"...", body:"...")
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article #redirect_to will cause the browser to make a new request. This needs to happen after mutating the databse or the app state
+    else
+      render :new, status: :unprocessable_entity
+      #render renders the speficed view for the current request
+    end
   end
 
-  # if @article.save
-  #   redirect_to @article
-  # else
-  #   render :new, status: :unprocessable_entity
-  # end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity #unprocessable_entity is a 422 status code
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other #see_other is a 303 status code
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
+
 # The index action retrieves all articles from the database and stores them in the @articles instance variable.
